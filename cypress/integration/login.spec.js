6f0b9f3a6a7490253122a0338cb5f4968b42429b
Galileo Guilherme <galileoguilherme@outlook.com>
2024-01-06 16:07:10 -0300

    aula 5

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -1,4 +1,5 @@
 ///<reference types="cypress" />
+const perfil = require('../fixtures/perfil.json')
 
 context('Funcionalidade Login', () => {
 
@@ -15,6 +16,22 @@ context('Funcionalidade Login', () => {
         cy.get('#password').type('teste@teste.com')
         cy.get('.woocommerce-form > .button').click()
         cy.get('.page-title').should('contain', 'Minha conta')
+    })
+
+    it('Deve fazer login com sucesso usando arquivo de dados', () => {
+        cy.get('#username').type(perfil.usuario)
+        cy.get('#password').type(perfil.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+    })
+
+    it.only('Deve fazer login com sucesso usando fixture', () => {
+        cy.fixture('perfil').then(dados => {
+            cy.get('#username').type(dados.usuario)
+        cy.get('#password').type(dados.senha)
+        cy.get('.woocommerce-form > .button').click()
+        cy.get('.page-title').should('contain', 'Minha conta')
+        })
     })
 
     it('Deve exibir uma mensagem de erro ao inserir email inválido', () => {
